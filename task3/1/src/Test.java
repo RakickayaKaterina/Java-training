628c7de0da7f82b91708654d980babb9f6105346
RakickayaKaterina <ultrareality023@gmail.com>
2017-10-18 01:27:01 +0300

    changed task 3.1

--- task3/1/src/Test.java
+++ task3/1/src/Test.java
@@ -1,12 +1,14 @@
 public class Test {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		String text = "Hello, I am Kate.       I wrote this class .";
-		Parser parser = new Parser(text);
 		System.out.println("Words: ");
-		for (String string : parser.parse()) {
+		for (String string : parse(text)) {
 			System.out.println(string);
 		}
+	}
+
+	public static String[] parse(String text) {
+		return text.split("[\\p{P}\\s]+");
 	}
 }
