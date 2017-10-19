66e1607d386b0dca7fd38b5ff0aa4d81e3ff3db7
RakickayaKaterina <ultrareality023@gmail.com>
2017-10-19 18:37:50 +0300

    changed class Printer

--- task3/2/src/Printer.java
+++ task3/2/src/Printer.java
@@ -1,9 +1,10 @@
 
 public class Printer {
 	public static void show(String message) {
-		System.out.println(message +" ");
+		System.out.println(message);
 	}
+
 	public static void show(int number) {
-		System.out.println(number +" ");
+		System.out.println(number);
 	}
 }
