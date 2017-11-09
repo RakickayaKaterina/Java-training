e9ba0206c8f60b9e64633bc40a3b7d953364ab48
RakickayaKaterina <ultrareality023@gmail.com>
2017-11-10 01:30:18 +0300

    added comparators

---
+++ task5/View/src/com/senla/rakickaya/view/comparators/course/LectorNameComparator.java
@@ -0,0 +1,21 @@
+package  com.senla.rakickaya.view.comparators.course;
+
+import java.util.Comparator;
+
+import com.senla.rakickaya.model.beans.Course;
+
+
+public class LectorNameComparator implements Comparator<Course> {
+
+	@Override
+	public int compare(Course o1, Course o2) {
+		if (o1 != null && o2 != null) {
+			return o1.getLector().getName().compareTo(o2.getLector().getName());
+		} else if (o1 != null && o2 == null) {
+			return 1;
+		} else {
+			return -1;
+		}
+	}
+
+}
