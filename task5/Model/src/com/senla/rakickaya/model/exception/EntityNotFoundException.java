6c22e3571f54f0b3c75ecb7eaa9b476455829dd2
RakickayaKaterina <ultrareality023@gmail.com>
2017-11-10 01:30:38 +0300

    addedmy Exception

---
+++ task5/Model/src/com/senla/rakickaya/model/exception/EntityNotFoundException.java
@@ -0,0 +1,30 @@
+package com.senla.rakickaya.model.exception;
+
+public class EntityNotFoundException extends Exception {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public EntityNotFoundException() {
+		super();
+	}
+
+	public EntityNotFoundException(String arg0, Throwable arg1, boolean arg2, boolean arg3) {
+		super(arg0, arg1, arg2, arg3);
+	}
+
+	public EntityNotFoundException(String arg0, Throwable arg1) {
+		super(arg0, arg1);
+	}
+
+	public EntityNotFoundException(String arg0) {
+		super(arg0);
+	}
+
+	public EntityNotFoundException(Throwable arg0) {
+		super(arg0);
+	}
+
+}
