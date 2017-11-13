97600f2da9272b0ebe05b5245648cc49c96d50fe
RakickayaKaterina <ultrareality023@gmail.com>
2017-11-13 17:01:26 +0300

    added enums

---
+++ task5/courseplanner-api/src/com/senla/rakickaya/courseplanner/api/data_exchange/enums/TagsRequest.java
@@ -0,0 +1,5 @@
+package com.senla.rakickaya.courseplanner.api.data_exchange.enums;
+
+public enum TagsRequest {
+	studentName, lectorName, lectureName, courseName, descriptionCourse, startDateCourse, endDateCourse, idStudent, idCourse, idLector, idLecture, date, startDate, endDate;
+}
