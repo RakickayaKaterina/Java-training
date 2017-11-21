eda027f382e90efd2c6f9960a5d092d4ce3fcca0
RakickayaKaterina <ultrareality023@gmail.com>
2017-11-21 13:07:21 +0300

    add kind of part csv

---
+++ 6/courseplanner-csv/src/com/senla/rakickaya/courseplanner/csv/Separators.java
@@ -0,0 +1,7 @@
+package com.senla.rakickaya.courseplanner.csv;
+
+public class Separators {
+	public static final String SEPARATOR_FIELDS=",";
+	public static final String SEPARATOR_ARRAYS=":";
+	public static final String SEPARATOR_OBJECT=" ";
+}
